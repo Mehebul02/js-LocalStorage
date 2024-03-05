6f189b0eaa24775ddd660cb40ee7a13d50f889e9
Mehebul02 <mehebul2123@gmail.com>
2024-03-05 23:16:41 +0600

    Localstorage data save

--- card.js
+++ card.js
@@ -1,10 +1,57 @@
+const productNameField = document.getElementById("product-name");
+const productQuantityField =document.getElementById("product-quantity");
+const productQuantityColor =document.getElementById("product-color");
 const addProduct = () => {
-  const productNameField = document.getElementById("product-name");
-  const productQuantityField =
-    document.getElementById("product-quantity");
   const product = productNameField.value;
   const quantity = productQuantityField.value;
+  const color= productQuantityColor.value;
   productNameField.value=''
   productQuantityField.value=''
-  console.log(product,quantity)
+  productQuantityColor.value=''
+  console.log(product,quantity,color)
+  handleSearch(product,quantity,color)
+  getTotalLocalStorage(product,quantity,color)
 };
+
+const handleSearch=(name,number,color)=>{
+  const tableData = document.getElementById('table_data');
+  const div = document.createElement('div');
+  div.innerHTML =` <table class="table">
+  <!-- head -->
+  <thead>
+    <tr>
+      <th></th>
+      <th></th>
+      <th></th>
+      <th></th>
+    </tr>
+  </thead>
+  <tbody>
+    <!-- row 1 -->
+    <tr>
+      <th>1</th>
+      <td>${name}</td>
+      <td>${number}</td>
+      <td>${color}</td>
+    </tr>
+  </tbody>
+</table>
+</div>`;
+tableData.appendChild(div)
+};
+const getStoredShoppingCart =()=>{
+  let cart ={};
+  const storCart =localStorage.getItem('cart');
+  if(storCart){
+   cart= JSON.parse(storCart)
+  }
+  return cart
+
+}
+const getTotalLocalStorage =(product,quantity,color) =>{
+  const cart = getStoredShoppingCart();
+  cart[product]=quantity,color;
+  const cartStringified =JSON.stringify(cart);
+  localStorage.setItem('cart',cartStringified);
+
+}
